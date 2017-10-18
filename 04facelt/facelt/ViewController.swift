53d9a0a3feb9b7bc1ae39b2887f429adebd72eda
dsfsfrec <gatsbychao@gmail.com>
2017-10-18 10:05:30 +0800

    setup

---
+++ 04facelt/facelt/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  facelt
+//
+//  Created by 曹影 on 2017/10/17.
+//  Copyright © 2017年 曹影. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+}
+
---
+++ 05faceltMVC/facelt/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  facelt
+//
+//  Created by Gatsby on 2017/10/17.
+//  Copyright © 2017年 Gatsby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var faceView: FaceView!
+    
+    
+}
+
